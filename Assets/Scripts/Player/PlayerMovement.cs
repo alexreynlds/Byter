98008e12f923fbb7a11c5b64c00be7477b095237
alex <ajrey@live.co.uk>
2022-12-27 00:34:00 +0000

    REWRITE PROGRESS ---------------- > Player Movement & Collision > Player Animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,14 +5,92 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public ContactFilter2D movementFilter;
+
+    public float collisionOffset = 0.01f;
+
+    public Sprite[] sprites;
+
     Vector2 movementInput;
+
+    float moveSpeed;
+
+    Rigidbody2D rb;
+
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
-        transform
-            .Translate(movementInput *
-            GetComponent<PlayerStats>().moveSpeed *
-            Time.deltaTime);
+        moveSpeed = GetComponent<PlayerStats>().moveSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        if (movementInput != Vector2.zero)
+        {
+            bool success = TryMove(movementInput);
+
+            if (!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
+
+                if (!success)
+                    success = TryMove(new Vector2(0, movementInput.y));
+            }
+        }
+
+        if (movementInput.x > 0)
+        {
+            spriteRenderer.sprite = sprites[1];
+            spriteRenderer.flipX = false;
+        }
+        else if (movementInput.x < 0)
+        {
+            spriteRenderer.sprite = sprites[1];
+            spriteRenderer.flipX = true;
+        }
+        else if (movementInput.y > 0)
+        {
+            spriteRenderer.sprite = sprites[2];
+        }
+        else if (movementInput.y < 0)
+        {
+            spriteRenderer.sprite = sprites[0];
+        }
+        else
+        {
+            spriteRenderer.sprite = sprites[0];
+        }
+    }
+
+    private bool TryMove(Vector2 direction)
+    {
+        int count =
+            rb
+                .Cast(direction,
+                movementFilter,
+                castCollisions,
+                moveSpeed * Time.fixedDeltaTime + collisionOffset);
+
+        if (count == 0)
+        {
+            rb
+                .MovePosition(rb.position +
+                direction * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     void OnMove(InputValue movementVal)
