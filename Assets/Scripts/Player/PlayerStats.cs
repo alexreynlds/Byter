c9574dbf3ad742f2a49804c248fba86c7814884b
alexreynlds <ajrey@live.co.uk>
2023-04-02 15:52:32 +0100

    Primitive movement system and placeholder sprites

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    //
+    public int health = 100;
+
+    public int maxHealth = 100;
+
+    public float moveSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
