cabdce3259b6795edfd44a75c1bf62bc5a1e9c22
alex <ajrey@live.co.uk>
2023-04-18 02:24:19 +0100

    Added full options menu functionality: graphics, resolution, fullscreen, audio + reset, still need to do saving to playerprefs

--- Assets/Scripts/Menus/MainMenuScript.cs
+++ Assets/Scripts/Menus/MainMenuScript.cs
@@ -2,20 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class MainMenuScript : MonoBehaviour
 {
     [SerializeField]
     private AudioSource clickSound;
+        [SerializeField]
+    private AudioSource menuMusic;
+    
+    [Header("Options Menu")]
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider musicVolumeSlider;
+    [SerializeField] private Slider sfxVolumeSlider;
+    [SerializeField] private Dropdown resolutionDropdown;
+    [SerializeField] private Dropdown graphicsDropdown;
+    [SerializeField] private Toggle fullscreenToggle;
 
-    // Start is called bdawdawdefore the first frame update
-    void Start()
+    private Resolution[] resolutions;
+    private int currentResolutionIndex = 0;
+    
+
+    private void Start()
     {
+        if (menuMusic != null)
+        {
+            menuMusic.Play();
+        }
+
+        // Setup the resolution dropdown
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "hz";
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        graphicsDropdown.value = QualitySettings.GetQualityLevel();
+        graphicsDropdown.RefreshShownValue();
+
+        
+
+        SetSFXVolume(-40);
+        SetMusicVolume(-40);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    public void SetSFXVolume(float volume){
+        audioMixer.SetFloat("SFXVol", volume);
+    }
+
+    public void SetMusicVolume(float volume){
+        audioMixer.SetFloat("musicVol", volume);
     }
 
     public void StartGame()
@@ -35,4 +85,42 @@ public class MainMenuScript : MonoBehaviour
             clickSound.Play();
         }
     }
+    
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution (int resIndex)
+    {
+        Resolution resolution = resolutions[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void ResetAudio()
+    {
+        musicVolumeSlider.value = -40;
+        sfxVolumeSlider.value = -40;
+    }
+
+    public void ResetGraphics()
+    {
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        SetFullscreen(true);
+        fullscreenToggle.isOn = true;
+
+
+        graphicsDropdown.value = 3;
+        graphicsDropdown.RefreshShownValue();
+
+    }
+
 }
