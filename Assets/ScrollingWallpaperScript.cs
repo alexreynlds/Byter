796a7719007ace56359459048502622779f9efa0
alexreynlds <ajrey@live.co.uk>
2023-05-01 20:17:15 +0100

    Added background to main menu

---
+++ Assets/ScrollingWallpaperScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingWallpaperScript : MonoBehaviour
+{
+    [SerializeField]
+    private RawImage image;
+
+    [SerializeField]
+    private float x,
+        y;
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.uvRect = new Rect(
+            image.uvRect.position + new Vector2(x, y) * Time.deltaTime,
+            image.uvRect.size
+        );
+    }
+}
